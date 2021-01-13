7799e2c4f5a71c8d35528ac514d73282dbb170f5
Javier <javmo94@gmail.com>
2021-01-13 20:32:58 -0300

    songscraper, songChordDB, python scripts

---
+++ scripts/python/scraping_preview.py
@@ -0,0 +1,43 @@
+
+import sys
+import json
+from os import system
+from song_dto import Song
+from lacuerdanetscrapper import parse_song_from
+
+url = sys.argv[1]
+
+
+try:
+    song=parse_song_from(url)
+    body = {
+        'title': song.title,
+        'artist': song.artist,
+        'lyric': {
+            'text': song.lyric_text
+        },
+        'lyricchord': song.chord_html
+    }
+    print(json.dumps(body))
+except Exception as e:
+    body = {
+        'title': "Oops! song not found or it's impossible to scrap",
+        'artist': "Oops! song not found",
+        'lyric': {
+            'text': "Oops! song not found"
+        },
+        'lyricchord': "Oops! song not found"
+    }
+    print(json.dumps(body))
+
+
+
+
+
+
+
+
+
+
+
+
