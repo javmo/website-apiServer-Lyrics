f241ea1059ff0fc3530e31148fa78dad008dca44
JAVIER MARTIN MORIXE <javiermartin.morixe@bbva.com>
2025-03-04 11:29:29 -0300

    mongoose changes

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
+// 🔹 Desactivar la restricción de populate en mongoose
+mongoose.set('strictPopulate', false);
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('DB is connected'))
     .catch(err => console.error('Connection error:', err));
+
 
